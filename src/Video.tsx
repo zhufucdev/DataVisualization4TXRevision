aaac57e8bef2b12c45700ac083a3777814a12661
zhufucdev <1095291434@qq.com>
2022-07-07 22:03:10 +0800

    Added separate composition for charts

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,5 +1,6 @@
 import { Composition } from 'remotion';
 import { DataVisualization } from './DataVisualization';
+import { TDMarkCharts } from './DataVisualization/3DMarkCharts';
 import { OScreen } from './DataVisualization/OverviewScreen';
 
 // Each <Composition> is an entry in the sidebar!
@@ -30,10 +31,16 @@ export const RemotionVideo: React.FC = () => {
 				fps={60}
 				width={1920}
 				height={1080}
-				// You can override these props for each render:
-				// https://www.remotion.dev/docs/parametrized-rendering
-				defaultProps={{
-				}}
+			/>
+			<Composition
+				// You can take the "id" to render a video:
+				// npx remotion render src/index.tsx <id> out/video.mp4
+				id="3DMarkCharts"
+				component={TDMarkCharts}
+				durationInFrames={180}
+				fps={60}
+				width={1920}
+				height={1080}
 			/>
 		</>
 	);
