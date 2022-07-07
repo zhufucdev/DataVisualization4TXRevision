96fcfcc548dd824c8334e3acff1ef4640f14fb78
zhufucdev <1095291434@qq.com>
2022-07-07 18:26:49 +0800

    Resolve renderer issues

--- src/DataVisualization/OverviewScreen.tsx
+++ src/DataVisualization/OverviewScreen.tsx
@@ -1,19 +1,57 @@
 import React from "react";
 import { AbsoluteFill, interpolate, Sequence, spring, useCurrentFrame, useVideoConfig } from "remotion";
-import { IMG_PREFIX, TEST_ITEMS } from "./constants";
+import { IMG_PREFIX } from "./constants";
 import { OItem } from "./OverviewItem";
 
-import "./Img/3dmark-time-spy-hero.jpg";
-import "./Img/3dmark-logo.png";
-import "./Img/forza.jpg";
-import "./Img/cyberpunk.jpg";
-import "./Img/odyssey.jpg";
+import tdCover from "./Img/3dmark-time-spy-hero.jpg";
+import tdLogo from "./Img/3dmark-logo.png";
+import forza from "./Img/forza.jpg";
+import cyberpunk from "./Img/cyberpunk.jpg";
+import odyssey from "./Img/odyssey.jpg";
+
+export const testItems = new Array(
+  {
+    title: '3DMark Time Spy',
+    subtitle: 'DX12 理论性能测试',
+    background: '#FF6F00',
+    cover: tdCover,
+    icon: tdLogo,
+    dark: true,
+  },
+  {
+    title: '极限竞速：地平线4',
+    subtitle: 'DX12 游戏测试',
+    background: '#BF360C',
+    cover: forza,
+    dark: true,
+  },
+  {
+    title: '赛博朋克2077',
+    subtitle: 'DX12 游戏及光追测试',
+    background: '#EEFF41',
+    cover: cyberpunk,
+    dark: false,
+  },
+  {
+    title: '刺客信条：奥德赛',
+    subtitle: 'DX12 游戏测试',
+    background: '#5D4037',
+    cover: odyssey,
+    icon: '',
+    dark: true,
+  },
+  {
+    title: 'Cinebench R23',
+    subtitle: 'CPU渲染测试',
+    background: '',
+    cover: '',
+    icon: '',
+    dark: false,
+  }
+);
+
 
 const itemsCount = 4, itemsInRow = 2, margin = 36, rows = Math.floor(itemsCount / itemsInRow);
-
-export function getAbsImgUrl(custom: string): string {
-  return custom ? `${IMG_PREFIX}/${custom}` : custom;
-}
 
 export const OScreen: React.FC = () => {
   const frame = useCurrentFrame();
@@ -65,12 +103,12 @@ export const OScreen: React.FC = () => {
       }
     }
 
-    const record = TEST_ITEMS[i];
+    const record = testItems[i];
     elements.push(
       <div style={{ width: `${100 / itemsInRow}%`, ...marginStyle, opacity, transform: `translateY(${translateY}px)` }}>
         <OItem title={record.title} subtitle={record.subtitle}
-        background={record.background} cover={getAbsImgUrl(record.cover)}
-        icon={getAbsImgUrl(record.icon)} dark={record.dark}/>
+        background={record.background} cover={record.cover}
+        icon={record.icon} dark={record.dark}/>
       </div>
     )
   }
--- src/DataVisualization/constants.ts
+++ src/DataVisualization/constants.ts
@@ -1,6 +1,2 @@
-import { TestItem } from "../../types/test";
-
 export const FONT_FAMILY = 'Noto Sans SC, SF Pro Text, Helvetica, Arial, sans-serif';
 export const CHART_STROKE = 10;
-
-export const IMG_PREFIX = './src/DataVisualization/Img';
