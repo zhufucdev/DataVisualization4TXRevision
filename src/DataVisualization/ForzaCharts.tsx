0c774f618db9883054447d635ad7b81e3ad9ec18
zhufucdev <1095291434@qq.com>
2022-07-13 09:22:49 +0800

    Adopt new way of expressing section

--- src/DataVisualization/ForzaCharts.tsx
+++ src/DataVisualization/ForzaCharts.tsx
@@ -14,9 +14,9 @@ function showAverageFPS(dataset: Table) {
   return new ShowAverage(
     (a) => `平均${a.toFixed(2)}fps`,
     {
-      column: dataset.cols[2],
-      from: dataset.data[4].cols[2],
-      to: dataset.data[dataset.data.length - 1].cols[2]
+      column: 2,
+      from: 2,
+      to: dataset.data.length - 1
     }
   );
 }
@@ -25,9 +25,9 @@ function showAverageTemp(dataset: Table, colIndex: number): ShowAverage {
   return new ShowAverage(
     (a) => `平均${a.toFixed(0)}℃`,
     {
-      column: dataset.cols[colIndex],
-      from: dataset.data[0].cols[colIndex],
-      to: dataset.data[test3.data.length - 1].cols[colIndex]
+      column: colIndex,
+      from: 0,
+      to: test3.data.length - 1
     }
   )
 }
@@ -36,9 +36,9 @@ test1.visualEffect = showAverageFPS(test1);
 test2.visualEffect = new ShowMaximun(
   (a) => `最高${a.toFixed(0)}%`,
   {
-    column: test2.cols[1],
-    from: test2.data[0].cols[1],
-    to: test2.data[test2.data.length - 1].cols[1]
+    column: 1,
+    from: 0,
+    to: test2.data.length - 1
   }
 )
 test3.visualEffect = showAverageFPS(test3);
@@ -57,43 +57,43 @@ export const ForzaCharts: React.FC<{}> = () => {
     <Sequence from={0} durationInFrames={140}>
       <AbsoluteFill style={container}>
         <LineChart width={width} height={height} source={test1} framesAwait={50}
-          transition={false} dark={true} primaryColor={color} label="基准测试" />
+          translation={false} dark={true} primaryColor={color} label="基准测试" />
       </AbsoluteFill>
     </Sequence>
     <Sequence from={140} durationInFrames={90}>
       <AbsoluteFill style={container}>
         <LineChart width={width} height={height} source={test1}
-          transition={true} dark={true} primaryColor={color} label="基准测试" />
+          translation={true} dark={true} primaryColor={color} label="基准测试" />
       </AbsoluteFill>
     </Sequence>
     <Sequence from={230} durationInFrames={90}>
       <AbsoluteFill style={container}>
         <LineChart width={width} height={height} source={test2}
-          transition={false} dark={true} primaryColor={color} label="实际操作" />
+          translation={false} dark={true} primaryColor={color} label="实际操作" />
       </AbsoluteFill>
     </Sequence>
     <Sequence from={320} durationInFrames={50}>
       <AbsoluteFill style={container}>
         <LineChart width={width} height={height} source={test2}
-          transition={true} dark={true} primaryColor={color} label="实际操作" />
+          translation={true} dark={true} primaryColor={color} label="实际操作" />
       </AbsoluteFill>
     </Sequence>
     <Sequence from={370} durationInFrames={50}>
       <AbsoluteFill style={container}>
         <LineChart width={width} height={height} source={test3}
-          transition={true} dark={true} primaryColor={color} label="实际操作" />
+          translation={true} dark={true} primaryColor={color} label="实际操作" />
       </AbsoluteFill>
     </Sequence>
     <Sequence from={420} durationInFrames={50}>
       <AbsoluteFill style={container}>
         <LineChart width={width} height={height} source={test4}
-          transition={true} dark={true} primaryColor={color} label="实际操作" />
+          translation={true} dark={true} primaryColor={color} label="实际操作" />
       </AbsoluteFill>
     </Sequence>
     <Sequence from={470}>
       <AbsoluteFill style={container}>
         <LineChart width={width} height={height} source={test5}
-          transition={true} dark={true} primaryColor={color} label="实际操作" />
+          translation={true} dark={true} primaryColor={color} label="实际操作" />
       </AbsoluteFill>
     </Sequence>
   </>
